86d024e9a12a11619b85cf073a1ffdc1b55b8ade
paval <pkasko-ua@singlestore.com>
2021-07-03 00:15:31 +0300

    Modify setup script for dots

--- scripts/dotsetup.sh
+++ scripts/dotsetup.sh
@@ -8,9 +8,9 @@ alias dots='git --git-dir=$HOME/dots/ --work-tree=$HOME'
 if ! dots checkout; then
     echo "Checked out config."
 else
-    mkdir -p .config-backup
+    mkdir -p ~/.config-backup
     echo "Backing up pre-existing dot files."
-    dots checkout 2>&1 | grep -E "\\s+\\." | awk '{print $1}' | xargs -I{} mv {} .config-backup/{}
+    dots checkout 2>&1 | grep -E "\\s+\\." | awk '{print $1}' | xargs -I{} mv {} ~/.config-backup/{}
 fi
 
 dots checkout
