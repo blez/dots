4d6345597729f363d01f025a82cd975b25c988a4
paval <pkasko-ua@singlestore.com>
2021-07-13 23:50:04 +0300

    Update install emacs script

--- scripts/emacs-install.sh
+++ scripts/emacs-install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git clone git://git.savannah.gnu.org/emacs.git -b feature/native-comp
+git clone git://git.savannah.gnu.org/emacs.git
 cd emacs
 make clean
 ./autogen.sh
 ./configure --with-modules --with-native-compilation --with-json --without-pop --with-mailutils
+# make bootstrap
 make -j$(nproc)
 make install
