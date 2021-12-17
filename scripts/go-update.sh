f16046783ecc2244fa2eb849411077b0e2dc0b69
paval <pkasko-ua@singlestore.com>
2021-12-17 11:20:46 +0200

    Add go update script

---
+++ scripts/go-update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+filename=$(curl https://go.dev/dl/?mode=json | jq '.[0] .files' | jq -r '.[] | select(.os == "linux" and .arch == "amd64") | .filename')
+wget -O ~/Downloads/go.tar.gz "https://go.dev/dl/$filename"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf ~/Downloads/go.tar.gz
+rm ~/Downloads/go.tar.gz
+echo "go was updated to $(go version)"
