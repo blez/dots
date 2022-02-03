289ea846698d4c5005c142ebcbc23a943cc9ce14
paval <pkasko-ua@singlestore.com>
2022-02-03 16:15:37 +0200

    Change wrap symbols

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -379,9 +379,9 @@ main = do
               <+> dynamicLogWithPP
                 xmobarPP
                   { ppOutput = hPutStrLn xmproc,
-                    ppCurrent = xmobarColor "red" "" . wrap "> " "", -- Current workspace in xmobar
+                    ppCurrent = xmobarColor "red" "" . wrap "→ " "", -- Current workspace in xmobar
                     ppVisible = xmobarColor "white" "", -- Visible but not current workspace
-                    ppHidden = xmobarColor "white" "" . wrap "|" "", -- Hidden workspaces in xmobar
+                    ppHidden = xmobarColor "white" "" . wrap "" "°", -- Hidden workspaces in xmobar
                     ppHiddenNoWindows = xmobarColor "white" "", -- Hidden workspaces (no windows)
                     ppSep = " | ", -- Separators in xmobar
                     ppUrgent = xmobarColor "#C45500" "" . wrap "!" "!", -- Urgent workspace
