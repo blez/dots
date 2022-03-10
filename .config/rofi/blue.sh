f5f1b1ab09591b7b08f08adcf6629d0e8fb5e129
paval <pkasko-ua@singlestore.com>
2022-03-10 20:31:58 +0200

    Add bluetooth script for rofi

---
+++ .config/rofi/blue.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+main() {
+    declare -A devices=()
+
+    rawDevices=$(bluetoothctl devices | awk '{$1=""; print $0}' | awk '{$1=$1;print}')
+
+    while IFS= read -r line; do
+        id=$(echo "$line" | awk '{print $1}')
+        name=$(echo "$line" | awk '{$1=""; print $0}' | awk '{$1=$1;print}')
+
+        if [ "$(bluetoothctl info "$id" | grep Connected: | awk '{print $2}')" == "yes" ]; then
+            devices["* $name"]="$id"
+        else
+            devices["$name"]="$id"
+        fi
+    done <<<"$rawDevices"
+
+    device=$( (for dev in "${!devices[@]}"; do echo "$dev"; done) | rofi -dmenu -matching fuzzy -no-custom -location 0 -i -p "Bluetooth devices")
+
+    if [ -n "$device" ]; then
+        if echo "$device" | grep -q "\*"; then
+            bluetoothctl disconnect "${devices[$device]}" || notify-send --app-name "Rofi" "Failed to disconnected from $device"
+            notify-send --app-name "Rofi" "Disconnected from $device"
+        else
+            bluetoothctl connect "${devices[$device]}" || notify-send --app-name "Rofi" "Failed to connected to $device"
+            notify-send --app-name "Rofi" "Connected to $device"
+        fi
+    else
+        exit
+    fi
+}
+
+main
+
+exit 0
