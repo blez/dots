4ef94a1260e602c960bc636e67fcbf2faf57a31d
paval <pkasko-ua@singlestore.com>
2022-09-05 13:10:16 +0300

    Fix tsupdate

--- scripts/tsupdate.sh
+++ scripts/tsupdate.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-wget -O ~/Downloads/telegram.tar.xz https://telegram.org/dl/desktop/linux
+tag=$(curl https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest | jq -r .tag_name)
+wget -O ~/Downloads/telegram.tar.xz "https://github.com/telegramdesktop/tdesktop/releases/download/${tag}/tsetup.${tag#v}.tar.xz"
 tar -xf ~/Downloads/telegram.tar.xz -C ~/Downloads
 sudo mv ~/Downloads/Telegram/* /usr/local/bin
 rm ~/Downloads/telegram.tar.xz
