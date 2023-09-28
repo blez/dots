e27a2f38f8897d8ba780fa9144ff569c6d088494
paval <pkasko-ua@singlestore.com>
2023-09-28 16:54:20 +0300

    Fix telegram-update script

--- scripts/tsupdate.sh
+++ scripts/tsupdate.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-tag=$(curl https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest | jq -r .tag_name)
+url=$(curl https://api.github.com/repos/telegramdesktop/tdesktop/releases |
+    jq -r 'first(.[] | .assets[] | select(.label == "Linux 64 bit: Binary")) | .browser_download_url')
 
-wget -O ~/Downloads/telegram.tar.xz "https://updates.tdesktop.com/tlinux/tsetup.${tag#v}.tar.xz"
-#wget -O ~/Downloads/telegram.tar.xz "https://github.com/telegramdesktop/tdesktop/releases/download/${tag}/tsetup.${tag#v}.tar.xz"
+wget -O ~/Downloads/telegram.tar.xz "$url"
 tar -xf ~/Downloads/telegram.tar.xz -C ~/Downloads
 sudo mv ~/Downloads/Telegram/* /usr/local/bin
+
 rm ~/Downloads/telegram.tar.xz
 rm -rf ~/Downloads/Telegram
-echo "Telegram was updated"
+
+version=$(awk -F '/' '{print $8}' <<< "$url")
+echo "Telegram was updated to $version"
