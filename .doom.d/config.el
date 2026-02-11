19d2df3ee4aa8d9b7d11695f804eb07b3ddbab25
paval <pkasko-ua@singlestore.com>
2026-02-11 20:02:04 +0200

    shfmt

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -20,7 +20,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "JetBrains Mono Medium" :size 20))
 ;; (setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 23))
-(setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 30))
+(setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 25))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
--- scripts/go-utils.sh
+++ scripts/go-utils.sh
@@ -10,3 +10,4 @@ go install golang.org/x/tools/cmd/godoc@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install golang.org/x/tools/cmd/gorename@latest
 go install golang.org/x/tools/cmd/guru@latest
+go install mvdan.cc/sh/v3/cmd/shfmt@latest
