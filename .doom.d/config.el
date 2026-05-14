468de5753804b1f30fb60c60c4015172b6f817f0
paval <pkasko-ua@singlestore.com>
2026-05-14 18:39:46 +0300

    fmt

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,7 +6,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "blez"
-      user-mail-address "pavalk6@gmail.com")
+    user-mail-address "pavalk6@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -33,38 +33,38 @@
 
 ;; (server-start)
 (after! server
-  (unless (server-running-p)
-    (server-start)))
+    (unless (server-running-p)
+        (server-start)))
 
 (map! :after neotree
-      :map neotree-mode-map
-      :m "h" #'+neotree/collapse-or-up)
+    :map neotree-mode-map
+    :m "h" #'+neotree/collapse-or-up)
 (map! :after neotree
-      :map neotree-mode-map
-      :m "l" #'+neotree/expand-or-open)
+    :map neotree-mode-map
+    :m "l" #'+neotree/expand-or-open)
 (map! :i "M-p" #'yank)
 (map! :i "C-j" #'next-line)
 (map! :i "C-k" #'previous-line)
 (map! :n "<f5>" #'call-last-kbd-macro)
 
 (after! lsp-mode
-  (setq lsp-diagnostics-provider :flycheck))
+    (setq lsp-diagnostics-provider :flycheck))
 
 (after! magit
-  (set-popup-rule! "^\\*magit-revision:" :side 'bottom :size 0.7))
+    (set-popup-rule! "^\\*magit-revision:" :side 'bottom :size 0.7))
 
 (map! :after flycheck
-      :map flycheck-mode-map
-      :m "<f12>" #'flycheck-next-error
-      :m "<f9>"  #'flycheck-previous-error
-      :m "<f11>" #'flycheck-list-errors
-      :m "<f6>"  #'flycheck-buffer)
+    :map flycheck-mode-map
+    :m "<f12>" #'flycheck-next-error
+    :m "<f9>"  #'flycheck-previous-error
+    :m "<f11>" #'flycheck-list-errors
+    :m "<f6>"  #'flycheck-buffer)
 (map! :map lsp-mode-map
-      :m "<f7>" #'lsp-ui-doc-show)
+    :m "<f7>" #'lsp-ui-doc-show)
 
 (eval-after-load 'smerge-mode
-  (lambda ()
-    (define-key smerge-mode-map (kbd "C-,") smerge-basic-map)))
+    (lambda ()
+        (define-key smerge-mode-map (kbd "C-,") smerge-basic-map)))
 
 (global-unset-key (kbd "<f3>"))
 (map! :m "<f3>" #'lsp-workspace-restart)
@@ -74,30 +74,30 @@
 
 (map! :leader :desc "Transpose frame" "w /" #'transpose-frame)
 (map! :leader
-      (:prefix-map ("a" . "Custom")
-       :desc "Save buffer" "a" #'save-buffer))
+    (:prefix-map ("a" . "Custom")
+        :desc "Save buffer" "a" #'save-buffer))
 (map! :ie "C-h" #'backward-delete-char-untabify)
 (map! :ie "C-d" #'delete-char)
 (map! :n "C-e" #'doom/forward-to-last-non-comment-or-eol)
 
 (after! dired
-  (map! :map dired-mode-map
+    (map! :map dired-mode-map
         :n "-" #'dired-do-kill-lines))
 
 (defun current-line-empty-p ()
-  (save-excursion
-    (beginning-of-line)
-    (looking-at-p "[[:space:]]*$")))
+    (save-excursion
+        (beginning-of-line)
+        (looking-at-p "[[:space:]]*$")))
 (defun move-previous-line ()
-  (interactive)
-  (forward-line -1)
-  (if (current-line-empty-p)
-      (kill-line)))
+    (interactive)
+    (forward-line -1)
+    (if (current-line-empty-p)
+        (kill-line)))
 (defun move-next-line ()
-  (interactive)
-  (beginning-of-line)
-  (+evil/insert-newline-above 1)
-  (forward-line 1))
+    (interactive)
+    (beginning-of-line)
+    (+evil/insert-newline-above 1)
+    (forward-line 1))
 
 (map! :m "C-j" #'move-next-line)
 (map! :m "C-k" #'move-previous-line)
@@ -106,18 +106,18 @@
 (map! :m "C-M-k" #'evil-scroll-line-up)
 
 (use-package! drag-stuff
-  :defer t
-  :init
-  (map! "M-K"    #'drag-stuff-up
+    :defer t
+    :init
+    (map! "M-K"    #'drag-stuff-up
         "M-J"  #'drag-stuff-down
         "M-H"  #'drag-stuff-left
         "M-L" #'drag-stuff-right))
 
 (defun previous-func ()
-  (interactive)
-  (end-of-defun)
-  (end-of-defun)
-  (beginning-of-defun))
+    (interactive)
+    (end-of-defun)
+    (end-of-defun)
+    (beginning-of-defun))
 (map! :m "M-j" #'previous-func)
 (map! :m "M-k" #'beginning-of-defun)
 (map! :n "," #'evil-first-non-blank)
@@ -128,128 +128,128 @@
 
 ;; corfu
 (after! corfu
-  (setq corfu-preselect 'first
+    (setq corfu-preselect 'first
         corfu-cycle t      ;; Allows cycling from last candidate back to first
         corfu-auto t       ;; Enable auto-completion as you type
         corfu-auto-delay 0
         corfu-preview-current t
         corfu-preview-current nil)
 
-  (map! :map corfu-map
+    (map! :map corfu-map
         :i "TAB"        #'corfu-insert
         :i "<tab>"      #'corfu-insert
         :i "S-TAB"      #'corfu-previous
         :i "<backtab>"  #'corfu-previous)
-  )
+    )
 
 ;; setup lsp + other linters
 (setenv "GOPATH" (expand-file-name "~/go/"))
 (add-hook! 'lsp-after-initialize-hook
-  (run-hooks (intern (format "%s-lsp-hook" major-mode))))
+    (run-hooks (intern (format "%s-lsp-hook" major-mode))))
 (defun go-flycheck-setup ()
-  (message "Setting up flycheck for Go")
-  (flycheck-add-next-checker 'lsp 'golangci-lint))
+    (message "Setting up flycheck for Go")
+    (flycheck-add-next-checker 'lsp 'golangci-lint))
 (defun go-lsp-setup ()
-  (setq lsp-go-build-flags ["-tags=operator,integration,cluster,debug"]))
+    (setq lsp-go-build-flags ["-tags=operator,integration,cluster,debug"]))
 (add-hook! '(go-mode-lsp-hook go-ts-mode-lsp-hook) #'go-flycheck-setup #'go-lsp-setup)
 
 (add-hook! '(sh-mode-lsp-hook sh-ts-mode-lsp-hook) #'sh-flycheck-setup)
 (defun sh-flycheck-setup ()
-  (flycheck-add-next-checker 'lsp 'sh-bash 'sh-shellcheck))
+    (flycheck-add-next-checker 'lsp 'sh-bash 'sh-shellcheck))
 
 (add-hook! lsp-mode
-  (defalias '+lookup/references 'lsp-find-references))
+    (defalias '+lookup/references 'lsp-find-references))
 
 (add-hook! 'c-mode-hook
-  (if (equal "keymap.c" (file-name-nondirectory buffer-file-name))
-      (setq +format-with :none)))
+    (if (equal "keymap.c" (file-name-nondirectory buffer-file-name))
+        (setq +format-with :none)))
 
 (after! lsp-clangd
-  (setq lsp-clients-clangd-executable "/usr/bin/clangd"))
+    (setq lsp-clients-clangd-executable "/usr/bin/clangd"))
 
 (with-eval-after-load 'lsp-clangd
-  (setq lsp-clients-clangd-args
+    (setq lsp-clients-clangd-args
         '("-j=3"
-          "--background-index"
-          "--clang-tidy"
-          "--completion-style=detailed"
-          "--header-insertion=never"
-          "--header-insertion-decorators=0"))
-  (set-lsp-priority! 'clangd 2))
+             "--background-index"
+             "--clang-tidy"
+             "--completion-style=detailed"
+             "--header-insertion=never"
+             "--header-insertion-decorators=0"))
+    (set-lsp-priority! 'clangd 2))
 
 (use-package! makefile-executor
-  :config
-  (add-hook 'makefile-mode-hook 'makefile-executor-mode))
+    :config
+    (add-hook 'makefile-mode-hook 'makefile-executor-mode))
 
 (use-package! olivetti
-  :config
-  (add-hook! '(
-               go-mode-hook go-ts-mode-hook
-               sh-mode-hook sh-ts-mode-hook
-               dart-mode-hook dart-ts-mode-hook
-               yaml-mode-hook yaml-ts-mode-hook
-               org-mode-hook org-ts-mode-hook
-               rustic-mode-hook rustic-ts-mode-hook
-               python-mode-hook python-ts-mode-hook
-               c++-mode-hook c++-ts-mode-hook
-               emacs-lisp-mode-hook
-               ) #'olivetti-mode))
+    :config
+    (add-hook! '(
+                    go-mode-hook go-ts-mode-hook
+                    sh-mode-hook sh-ts-mode-hook
+                    dart-mode-hook dart-ts-mode-hook
+                    yaml-mode-hook yaml-ts-mode-hook
+                    org-mode-hook org-ts-mode-hook
+                    rustic-mode-hook rustic-ts-mode-hook
+                    python-mode-hook python-ts-mode-hook
+                    c++-mode-hook c++-ts-mode-hook
+                    emacs-lisp-mode-hook
+                    ) #'olivetti-mode))
 
 (setq-hook! 'olivetti-mode-hook olivetti-body-width 150)
 
 (use-package! dap-dlv-go
-  :after (go-mode dap-mode)
-  :config
-  (dap-register-debug-template "Remote Debug Go"
-                               (list :type "go"
-                                     :request "attach"
-                                     :mode "remote"
-                                     :name "Remote Debug Go"
-                                     :host "0.0.0.0"
-                                     :port 2345
-                                     :program "${workspaceFolder}")))
+    :after (go-mode dap-mode)
+    :config
+    (dap-register-debug-template "Remote Debug Go"
+        (list :type "go"
+            :request "attach"
+            :mode "remote"
+            :name "Remote Debug Go"
+            :host "0.0.0.0"
+            :port 2345
+            :program "${workspaceFolder}")))
 
 (use-package! dap-mode
-  :after lsp-mode
-  :config
-  (dap-mode 1)
-  (dap-ui-mode 1)
-  (dap-auto-configure-mode -1)
-  (dap-ui-controls-mode -1)
-  (dap-tooltip-mode -1)
-  (tooltip-mode -1)
-  (setq dap-auto-configure-features '())
-  (set-popup-rule! "^\\*Remote Debug Go.*" :ignore t)
-  (set-popup-rule! "^\\*Dlv Remote Debug" :ignore t)
-  ;; (set-popup-rule! "*Launch File" :size 0.2 :slot -4 :select t :quit t :ttl 0 :side 'bottom)
-  (set-popup-rule! "^\\*dap-ui-locals\\*" :side 'bottom)
-  (remove-hook 'dap-mode-hook #'dap-ui-mode)
-  (remove-hook 'dap-mode-hook #'dap-ui-controls-mode)
-  (add-hook 'dap-stopped-hook
-            (lambda (arg)
-              (interactive)
-              (dap-hydra)
-              ))
-  )
+    :after lsp-mode
+    :config
+    (dap-mode 1)
+    (dap-ui-mode 1)
+    (dap-auto-configure-mode -1)
+    (dap-ui-controls-mode -1)
+    (dap-tooltip-mode -1)
+    (tooltip-mode -1)
+    (setq dap-auto-configure-features '())
+    (set-popup-rule! "^\\*Remote Debug Go.*" :ignore t)
+    (set-popup-rule! "^\\*Dlv Remote Debug" :ignore t)
+    ;; (set-popup-rule! "*Launch File" :size 0.2 :slot -4 :select t :quit t :ttl 0 :side 'bottom)
+    (set-popup-rule! "^\\*dap-ui-locals\\*" :side 'bottom)
+    (remove-hook 'dap-mode-hook #'dap-ui-mode)
+    (remove-hook 'dap-mode-hook #'dap-ui-controls-mode)
+    (add-hook 'dap-stopped-hook
+        (lambda (arg)
+            (interactive)
+            (dap-hydra)
+            ))
+    )
 
 (setq-hook! 'rjsx-mode-hook +format-with-lsp nil)
 
 (setq lsp-grammarly-server-path "~/.local/share/pnpm/grammarly-languageserver")
 (use-package! lsp-grammarly
-  :after lsp-mode
-  :hook (text-mode . lsp))
+    :after lsp-mode
+    :hook (text-mode . lsp))
 
 (beacon-mode 1)
 
 (use-package! copilot
-  :hook (prog-mode . copilot-mode)
-  :bind (:map copilot-completion-map
+    :hook (prog-mode . copilot-mode)
+    :bind (:map copilot-completion-map
               ("C-e" . 'copilot-accept-completion)
               ("M-e" . 'copilot-accept-completion-by-word)
               ("C-n" . 'copilot-next-completion)
               ("C-p" . 'copilot-previous-completion))
-  :config
-  (add-to-list 'copilot-indentation-alist '(go-mode 2)))
+    :config
+    (add-to-list 'copilot-indentation-alist '(go-mode 2)))
 
 (put '+format-with 'safe-local-variable 'symbolp)
 ;; (set-formatter! 'shfmt '("shfmt", "-s" "-i" "4" "-ln" "bash") :modes '(sh-mode))
@@ -276,58 +276,58 @@
 (setq org-roam-directory "~/blez/roam/")
 
 (add-hook 'projectile-after-switch-project-hook
-          (lambda ()
-            (let ((root (projectile-project-root)))
-              (message "Current project root: %s" root)
-              (cond
-               ((string-prefix-p (expand-file-name "~/helios/singlestore.com/helios/") root)
-                (message "Renaming perspective to 'backend'")
-                (persp-rename "backend"))
-               ((string-prefix-p (expand-file-name "~/helios/singlestore.com/") root)
-                (message "Renaming perspective to 'operator'")
-                (persp-rename "operator"))))))
+    (lambda ()
+        (let ((root (projectile-project-root)))
+            (message "Current project root: %s" root)
+            (cond
+                ((string-prefix-p (expand-file-name "~/helios/singlestore.com/helios/") root)
+                    (message "Renaming perspective to 'backend'")
+                    (persp-rename "backend"))
+                ((string-prefix-p (expand-file-name "~/helios/singlestore.com/") root)
+                    (message "Renaming perspective to 'operator'")
+                    (persp-rename "operator"))))))
 
 (setq auth-sources '("~/.authinfo"))
 
 (setq org-latex-pdf-process
-      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+    '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 (setq org-latex-listings 'minted)
 
 (after! gptel
-  (require 'auth-source)
-  (setq gptel-api-key (auth-source-pick-first-password :host "api.openai.com")))
+    (require 'auth-source)
+    (setq gptel-api-key (auth-source-pick-first-password :host "api.openai.com")))
 
 (map! :leader
-      :prefix ("a" . "gptel/aider")
-      :desc "GPTel"      "i" #'gptel
-      :desc "GPTel send" "s" #'gptel-send
-      :desc "GPTel Menu" "m" #'gptel-menu)
+    :prefix ("a" . "gptel/aider")
+    :desc "GPTel"      "i" #'gptel
+    :desc "GPTel send" "s" #'gptel-send
+    :desc "GPTel Menu" "m" #'gptel-menu)
 
 (use-package! aidermacs
-  :after auth-source
-  :config
-  (setenv "ANTHROPIC_API_KEY" (auth-source-pick-first-password :host "api.anthropic.com"))
-  (setenv "OPENAI_API_KEY"    (auth-source-pick-first-password :host "api.openai.com"))
-  :custom
-  (aidermacs-use-architect-mode t)
-  (aidermacs-default-model "sonnet"))
+    :after auth-source
+    :config
+    (setenv "ANTHROPIC_API_KEY" (auth-source-pick-first-password :host "api.anthropic.com"))
+    (setenv "OPENAI_API_KEY"    (auth-source-pick-first-password :host "api.openai.com"))
+    :custom
+    (aidermacs-use-architect-mode t)
+    (aidermacs-default-model "sonnet"))
 
 (global-disable-mouse-mode)
 (setq disable-mouse-global-mode t)
 (mapc #'disable-mouse-in-keymap
-      (list evil-motion-state-map
-            evil-normal-state-map
-            evil-visual-state-map
-            evil-insert-state-map))
+    (list evil-motion-state-map
+        evil-normal-state-map
+        evil-visual-state-map
+        evil-insert-state-map))
 
 (setq erc-server "irc.libera.chat"
-      erc-nick "blez"
-      erc-user-full-name "blez"
-      erc-track-shorten-start 8
-      erc-autojoin-channels-alist '(("irc.libera.chat" "#systemcrafters"))
-      erc-kill-buffer-on-part t
-      erc-auto-query 'bury)
+    erc-nick "blez"
+    erc-user-full-name "blez"
+    erc-track-shorten-start 8
+    erc-autojoin-channels-alist '(("irc.libera.chat" "#systemcrafters"))
+    erc-kill-buffer-on-part t
+    erc-auto-query 'bury)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
